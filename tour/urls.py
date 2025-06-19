8f11dbe9b5221c0ffa95480d8d5bbee22fbdf545
Muli40 <anitamuli784@gmail.com>
2025-06-19 13:23:50 +0300

    muli

--- tour/urls.py
+++ tour/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from tour import views
 
 urlpatterns = [
-    path('', views.helo),
-    
+    path('', views.home,name='home'),
+    path('toursite/', views.offers,name='toursite'),
+    path('offers/', views.offers,name='offers'),
+    path('contact/', views.offers,name='contact'),
+    path('search/', views.offers,name='search'),
+    path('activities/', views.offers,name='activities'),
+    path('anit/', views.offers,name='anit'),
 ]
--- tour/views.py
+++ tour/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def helo(request):
-    return render(request, 'base.html')
+
+def home(request):
+        return render(request,'tour/toursite.html')
+
+def offers(request):
+        return render(request,'tour/offers.html')
+
+def contact(request):
+        return render(request,'tour/Contact.html')
+
+def search(request):
+        return render(request,'tour/search.html')
+
+def activities(request):
+        return render(request,'tour/activities.html')
+
+def anit(request):
+        return render(request,'tour/anit.html')
+
+def toursite(request):
+        return render(request,'tour/toursite.html')
+
